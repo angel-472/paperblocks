44f3a8d190543da4f8ab29f6e5b0e5c15fd114de
angel <108492882+angel-472@users.noreply.github.com>
2026-07-20 10:20:47 -0400

    Refactor addABlockOmg to use a loop for block creation and adjust velocity initialization

--- src/game/game.js
+++ src/game/game.js
@@ -16,16 +16,17 @@ export class Game {
     console.log(`Game: Loaded all assets, starting engine...`);
     await this.#engine._start();
 
-    this.addABlockOmg((16 * 1), 0);
-    this.addABlockOmg((16 * 2), 0);
-    this.addABlockOmg((16 * 3), 0);
-    this.addABlockOmg((16 * 4), 0);
-
     this.addTestBody();
+
+    for(let i = 0; i < 10; i++){
+      this.addABlockOmg(i, 0)
+    }
 
     this.#engine.getCamera().setZoom(4, 58, 0)
   }
-  addABlockOmg(canvasX, canvasY){
+  addABlockOmg(gridX, gridY){
+    const canvasX = gridX * 16;
+    const canvasY = gridY * 16;
     const ecs = this.#engine.getECS();
     const blockEntity = ecs.createEntity();
     ecs.addComponent(blockEntity, 'Transform', {x: canvasX, y: canvasY, rotation: 0, zIndex: 0, scale: {x: 1, y: 1}});
@@ -37,7 +38,9 @@ export class Game {
       const blockEntity = ecs.createEntity();
       ecs.addComponent(blockEntity, 'Transform', {x: 16 * 2, y: 16 * -2, rotation: 0, zIndex: 0, scale: {x: 1, y: 1}});
       ecs.addComponent(blockEntity, 'Sprite', {width: 16, height: 16, textureId: "no_sprite"});
-      ecs.addComponent(blockEntity, 'Velocity', {x: 0.75, y: 0, weight: 1, ignoreGravity: false});
+      const velocity = ecs.addComponent(blockEntity, 'Velocity', {x: 0, y: 0, weight: 1, ignoreGravity: false});
       ecs.addComponent(blockEntity, 'Collider', {width: 16, height: 16, friction: 0.85});
+
+      velocity.y = 10;
   }
 }
